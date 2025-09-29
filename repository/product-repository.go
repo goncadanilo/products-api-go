7e0af284006af80fa9530fed8c0ef43b26637897
Danilo Gonçalves <gonca.danilo@gmail.com>
2025-09-29 19:12:37 -0300

    feat: add product repository with get products method

---
+++ repository/product-repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/goncadanilo/products-api-go/model"
+)
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func NewProductRepository(connection *sql.DB) ProductRepository {
+	return ProductRepository{
+		connection: connection,
+	}
+}
+
+func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
+	query := "SELECT id, product_name, description, price, slug FROM product"
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
+
+	var productList []model.Product
+	var productObj model.Product
+
+	for rows.Next() {
+		err = rows.Scan(
+			&productObj.ID,
+			&productObj.Name,
+			&productObj.Description,
+			&productObj.Price,
+			&productObj.Slug)
+
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+
+		productList = append(productList, productObj)
+	}
+
+	rows.Close()
+
+	return productList, nil
+}
