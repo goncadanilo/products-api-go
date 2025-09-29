9002e86a3f12bac2502a534cf99b4cf65e16d43c
Danilo Gonçalves <gonca.danilo@gmail.com>
2025-09-29 19:13:44 -0300

    feat: update products controller to use products use-case

--- controller/product-controller.go
+++ controller/product-controller.go
@@ -4,25 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/goncadanilo/products-api-go/model"
+	"github.com/goncadanilo/products-api-go/usecase"
 )
 
 type productController struct {
+	productUseCase usecase.ProductUseCase
 }
 
-func NewProductController() productController {
-	return productController{}
+func NewProductController(usecase usecase.ProductUseCase) productController {
+	return productController{
+		productUseCase: usecase,
+	}
 }
 
 func (p *productController) GetProducts(ctx *gin.Context) {
-	products := []model.Product{
-		{
-			ID:          1,
-			Name:        "Teclado mecânico",
-			Description: "Teclado mecânico Keychron k3 v2",
-			Price:       800,
-			Slug:        "teclado-mecanico",
-		},
+	products, err := p.productUseCase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
 	}
 
 	ctx.JSON(http.StatusOK, products)
